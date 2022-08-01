8a34e64d645581f62d84d304d38ecca3c161f51e
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-08-02 03:24:54 +0900

    feat : 좌석 배열 & 좌석 렌더링

--- src/pages/SeatPage.js
+++ src/pages/SeatPage.js
@@ -12,12 +12,309 @@ import BottomModal from "../components/BottomModal";
 
 const SeatPage = () => {
   const [isOpen, setIsOpen] = useState(true);
+  const [bottomModal, setBottomModal] = useState(false);
+
+  const des = 10;
+
+  const [seats, setSeats] = useState([
+    {
+      id: 1,
+      left: "91px",
+      top: "56px",
+      seated: false,
+      mine: false,
+      station: 2,
+    },
+    { id: 2, left: "91px", top: "80px", seated: true, mine: false, station: 2 },
+    {
+      id: 3,
+      left: "91px",
+      top: "103px",
+      seated: false,
+      mine: false,
+      station: 2,
+    },
+    {
+      id: 4,
+      left: "91px",
+      top: "205px",
+      seated: true,
+      mine: false,
+      station: 2,
+    },
+    {
+      id: 5,
+      left: "91px",
+      top: "228px",
+      seated: false,
+      mine: false,
+      station: 2,
+    },
+    {
+      id: 6,
+      left: "91px",
+      top: "251px",
+      seated: true,
+      mine: false,
+      station: 2,
+    },
+    {
+      id: 7,
+      left: "91px",
+      top: "274px",
+      seated: false,
+      mine: false,
+      station: 2,
+    },
+    {
+      id: 8,
+      left: "91px",
+      top: "297px",
+      seated: true,
+      mine: false,
+      station: 2,
+    },
+    {
+      id: 9,
+      left: "91px",
+      top: "320px",
+      seated: true,
+      mine: false,
+      station: 2,
+    },
+    {
+      id: 10,
+      left: "91px",
+      top: "422px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 11,
+      left: "91px",
+      top: "445px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 12,
+      left: "91px",
+      top: "468px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 13,
+      left: "91px",
+      top: "491px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 14,
+      left: "91px",
+      top: "514px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 15,
+      left: "91px",
+      top: "537px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 16,
+      left: "91px",
+      top: "640px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 17,
+      left: "91px",
+      top: "663px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 18,
+      left: "91px",
+      top: "686px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+
+    {
+      id: 19,
+      left: "266px",
+      top: "58px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 20,
+      left: "266px",
+      top: "81px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 21,
+      left: "266px",
+      top: "105px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 22,
+      left: "266px",
+      top: "205px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 23,
+      left: "266px",
+      top: "228px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 24,
+      left: "266px",
+      top: "251px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 25,
+      left: "266px",
+      top: "274px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 26,
+      left: "266px",
+      top: "297px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 27,
+      left: "266px",
+      top: "320px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 28,
+      left: "266px",
+      top: "422px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 29,
+      left: "266px",
+      top: "445px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 30,
+      left: "266px",
+      top: "468px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 31,
+      left: "266px",
+      top: "491px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 32,
+      left: "266px",
+      top: "514px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 33,
+      left: "266px",
+      top: "537px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 34,
+      left: "266px",
+      top: "641px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 35,
+      left: "266px",
+      top: "664px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+    {
+      id: 36,
+      left: "266px",
+      top: "687px",
+      seated: true,
+      mine: true,
+      station: 2,
+    },
+  ]);
+
+  const handleModal = () => {
+    setIsOpen(false);
+  };
+
+  const SelectSeat = id => {
+    console.log("좌석 선택", id);
+    setBottomModal(true);
+  };
   return (
     <div>
       <Background />
 
       <TopBar />
-      {isOpen && <Modal onClick={() => setIsOpen(!isOpen)} />}
+      {isOpen && <Modal onClick={() => handleModal()} />}
 
       <Wrapper>
         <BackIcon />
@@ -25,11 +322,48 @@ const SeatPage = () => {
 
         <Num>2024</Num>
         <Train>
-          <Seat />
+          {seats.map(seat => {
+            if (seat.mine) {
+              // 내 자리
+              console.log("내자리");
+              return (
+                <Seat
+                  onClick={() => SelectSeat(seat.id)}
+                  key={seat.id}
+                  left={seat.left}
+                  top={seat.top}
+                  myseat
+                />
+              );
+            } else {
+              if (seat.seated) {
+                // 누가 앉음
+                return (
+                  <Seat
+                    onClick={() => SelectSeat(seat.id)}
+                    key={seat.id}
+                    left={seat.left}
+                    top={seat.top}
+                    seated
+                  >
+                    {des - seat.station}
+                  </Seat>
+                );
+              }
+            }
+            return (
+              <Seat
+                onClick={() => SelectSeat(seat.id)}
+                key={seat.id}
+                left={seat.left}
+                top={seat.top}
+              />
+            );
+          })}
         </Train>
       </Wrapper>
 
-      <BottomModal />
+      <BottomModal isOpen={bottomModal} setBottomModal={setBottomModal} />
     </div>
   );
 };
