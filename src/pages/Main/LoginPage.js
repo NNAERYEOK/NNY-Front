fae415ecfbbb3dd93ed2e9c9ccb255c315f0ca5a
deliminzoo <minjoo9903@naver.com>
2022-08-08 23:38:12 +0900

    fix: styled component로 수정

--- src/pages/Main/LoginPage.js
+++ src/pages/Main/LoginPage.js
@@ -11,94 +11,94 @@ const LoginPage = () => {
   };
   return (
     <>
-      <GlobalStyle />
+      <Background />
       <BackBtn />
-      <h3>로그인</h3>
-      <div className="formField">
+      <Title>로그인</Title>
+      <FormField>
         <input className="idInput" placeholder="아이디" type="login" />
         <input className="pwInput" placeholder="비밀번호" type="password" />
         <Button>로그인</Button>
-        <div className="btn">
+        <NavDiv>
           <p className="member" onClick={goRegister}>
             회원가입
           </p>
           <p className="find">비밀번호/아이디 찾기</p>
-        </div>
-      </div>
+        </NavDiv>
+      </FormField>
     </>
   );
 };
 
 export default LoginPage;
-
-const GlobalStyle = createGlobalStyle`
-  body{
-    background:#212226;
+const Background = createGlobalStyle`
+  body {
+  background-color: var( --background-black);
   }
-  h3{
-    color:white;
-    margin-left:8%;
-    font-weight: 400;
-    font-size: 20px;
-  }
-  .formField{
-    background: #FFFFFF;
-    box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 11px;
-    width:78%;
-    margin: 0 auto;
-    padding: 6%;
-    height: 610px;
-  }
-  .btn{
-    margin: 10px auto;
-    width:75%;
-    max-width: 300px;
-    background-color: red;
-  }
-  p{
+`;
+const Title = styled.p`
+  color: white;
+  margin-left: 8%;
+  font-weight: 400;
+  font-size: 20px;
+`;
+const NavDiv = styled.div`
+  margin: 10px auto;
+  width: 75%;
+  max-width: 300px;
+  background-color: red;
+  p {
     display: inline-block;
-    color:rgba(33, 34, 38, 0.7);
-    font-family: 'NanumSquareOTF';
+    color: rgba(33, 34, 38, 0.7);
+    font-family: "NanumSquareOTF";
     cursor: pointer;
     font-weight: 400;
     font-size: 8px;
   }
-  .member{
-  float:left;
+  .member {
+    float: left;
   }
-  .find{
-    float:right;
+  .find {
+    float: right;
   }
-  
-  input{
+`;
+
+const FormField = styled.div`
+  background: #ffffff;
+  box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 11px;
+  width: 78%;
+  margin: 0 auto;
+  padding: 6%;
+  height: 610px;
+
+  input {
     display: block;
     margin: 0 auto;
     border: none;
-    border-bottom: 1px solid #4FE0B6;
-    width:75%;
+    border-bottom: 1px solid #4fe0b6;
+    width: 75%;
     max-width: 300px;
-    height:30px;
+    height: 30px;
     font-weight: 400;
     font-size: 13px;
-    text-indent: 10px;;
+    text-indent: 10px;
   }
-  .idInput{
+  .idInput {
     margin-top: 124px;
-    margin-bottom:5%;
+    margin-bottom: 5%;
   }
-  .idInput:placeholder-shown{
+  .idInput:placeholder-shown {
     margin-top: 124px;
-    margin-bottom:5%;
-    background-image:url('./image/login.svg'); 
+    margin-bottom: 5%;
+    background-image: url("./image/login.svg");
     background-repeat: no-repeat;
     background-position: right;
   }
-  .pwInput{
+  .pwInput {
     margin-top: 33px;
   }
-  .pwInput:placeholder-shown{
-    background-image:url('./image/pw.svg'); 
+  .pwInput:placeholder-shown {
+    background-image: url("./image/pw.svg");
     margin-top: 33px;
     background-repeat: no-repeat;
     background-position: right;
--- src/pages/Main/RegisterPage.js
+++ src/pages/Main/RegisterPage.js
@@ -5,98 +5,98 @@ import BackBtn from "../../components/BackBtn";
 const RegisterPage = () => {
   return (
     <>
-      <GlobalStyle />
+      <Background />
       <BackBtn />
-      <h3>회원가입</h3>
-      <div className="formField">
+      <Title>회원가입</Title>
+      <FormField>
         <div>
-          <p>아이디</p>
+          <Label>아이디</Label>
           <input id="idInput" placeholder="아이디" type="login" />
         </div>
         <div>
-          <p>비밀번호</p>
+          <Label>비밀번호</Label>
           <input id="pwInput" placeholder="비밀번호" type="password" />
         </div>
         <div>
-          <p>비밀번호 확인</p>
+          <Label>비밀번호 확인</Label>
           <input id="RepwInput" placeholder="비밀번호 확인" type="password" />
         </div>
         <div>
-          <p>본인인증</p>
-          <button>인증하기</button>
+          <Label>본인인증</Label>
+          <button id="self">인증하기</button>
         </div>
         <Button>가입하기</Button>
-      </div>
+      </FormField>
     </>
   );
 };
 export default RegisterPage;
 
-const GlobalStyle = createGlobalStyle`
-  body{
-    background:#212226;
+const Background = createGlobalStyle`
+  body {
+  background-color: var( --background-black);
   }
-  h3{
-    color:white;
-    margin-left:8%;
-    font-weight: 400;
-    font-size: 20px;
-  }
-  .formField{
-    background: #FFFFFF;
-    box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 11px;
-    width:78%;
-    margin: 0 auto;
-    padding: 6%;
-    height: 610px;
-  }
-  .formField div{
-    width:75%;
+`;
+const Title = styled.p`
+  color: white;
+  margin-left: 8%;
+  font-weight: 400;
+  font-size: 20px;
+`;
+const Label = styled.p`
+  font-weight: 400;
+  font-size: 11px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+`;
+
+const FormField = styled.div`
+  background: #ffffff;
+  box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 11px;
+  width: 78%;
+  margin: 0 auto;
+  padding: 6%;
+  height: 610px;
+  div {
+    width: 75%;
     max-width: 300px;
-    margin:auto;
+    margin: auto;
     margin-bottom: 50px;
   }
-  
-  p{
-    font-weight: 400;
-    font-size: 11px;
-    margin-top:20px;
-    margin-bottom:10px ;
-  }
-  input{
+  input {
     display: block;
     margin: 0 auto;
     border: none;
-    border-bottom: 1px solid #4FE0B6;
-    width:100%;
-    height:30px;
+    border-bottom: 1px solid #4fe0b6;
+    width: 100%;
+    height: 30px;
     font-weight: 400;
     font-size: 10px;
-    text-indent: 5px;;
+    text-indent: 5px;
   }
-  #idInput:placeholder-shown{
-    background-image:url('./image/login.svg'); 
+  #idInput:placeholder-shown {
+    background-image: url("./image/login.svg");
     background-repeat: no-repeat;
     background-position: right;
   }
-  #pwInput:placeholder-shown{
-    background-image:url('./image/pw.svg'); 
+  #pwInput:placeholder-shown {
+    background-image: url("./image/pw.svg");
     background-repeat: no-repeat;
     background-position: right;
   }
-  #RepwInput:placeholder-shown{
-    background-image:url('./image/repw.svg');
+  #RepwInput:placeholder-shown {
+    background-image: url("./image/repw.svg");
     background-repeat: no-repeat;
     background-position: right;
   }
-  button{
+  #self {
     width: 82px;
     height: 27px;
-    border:1px solid #4FE0B6; 
+    border: 1px solid #4fe0b6;
     border-radius: 10px;
-    background :none ;
+    background: none;
     font-size: 9px;
     cursor: pointer;
   }
-  `;
+`;
