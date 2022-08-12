be5cf8868805ca3b9433cc1ba90b9fee002c5abb
yun5581 <lonnie513@ewhain.net>
2022-08-13 05:56:26 +0900

    feat: 네브버튼 선택시 색 변경 기능 구현

--- src/pages/EyeHistoryPage/EyeHistoryPage.js
+++ src/pages/EyeHistoryPage/EyeHistoryPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, css } from "styled-components";
 import BackBtn from "../../components/BackBtn";
 import { Title } from "../ChargingPage";
 import UsedEye from "./usedEyeHistory";
@@ -7,41 +7,33 @@ import ChargedEye from "./chargedEyeHistory";
 import { useState, useEffect } from "react";
 
 export function EyeHistoryPage() {
-  const [pageSwitch, setPageSwitch] = useState(true);
-  const [btnActive, setBtnActive] = useState("1px solid #4FE0B6");
+  const [btnActive, setBtnActive] = useState(true);
 
   return (
     <>
       <GlobalStyle />
       <BackBtn />
       <Title>eye 내역</Title>
+      {/* {btnActive ? <historyNavBtn1 /> : <historyNavBtn2 />}{" "} */}
       <div className="historyBox">
         <div className="historyNav">
-          <button
-            className="historyNavBtn"
-            style={{ borderBottom: { btnActive } }}
-            onClick={() => {
-              setPageSwitch(true);
-              setBtnActive("8px solid #4FE0B6");
-              console.log(btnActive);
-            }}
-          >
-            eye 충전 내역
-          </button>
-          <button
-            className="historyNavBtn"
-            style={{ borderBottom: { btnActive } }}
-            onClick={() => {
-              setPageSwitch(false);
-              setBtnActive("8px solid #4FE0B6");
-              console.log(btnActive);
-            }}
-          >
-            eye 사용 내역
-          </button>
+          <BoxWrapper>
+            <Btn
+              className={btnActive ? "Active" : "NotActive"}
+              onClick={() => setBtnActive(!btnActive)}
+            >
+              eye 충전 내역{" "}
+            </Btn>
+            <Btn
+              className={btnActive ? "NotActive" : "Active"}
+              onClick={() => setBtnActive(!btnActive)}
+            >
+              eye 사용 내역{" "}
+            </Btn>
+          </BoxWrapper>
         </div>
         <div className="historyView">
-          {pageSwitch ? <ChargedEye /> : <UsedEye />}{" "}
+          {btnActive ? <ChargedEye /> : <UsedEye />}{" "}
         </div>
       </div>
     </>
@@ -49,27 +41,38 @@ export function EyeHistoryPage() {
 }
 
 const GlobalStyle = createGlobalStyle`
-
 .historyNav{
   width: 65%;
   margin: 0 auto 12%;
 }
-
-.historyNavBtn{
-  box-sizing: border-box;
-  width: 50%;
-  border: none;
-  background-color: #ffffff;
-  font-family: "NanumSquare";
-  font-size: 14px;
-  font-weight: 400;
-  padding: 25% 8% 4%;
-  /* border-bottom: 1px solid #4FE0B6; */
-}
-
-#selected{
-  background-color: tomato;
-}
 `;
+
+const BoxWrapper = styled.div`
+  .Active {
+    box-sizing: border-box;
+    width: 50%;
+    border: none;
+    background-color: #ffffff;
+    font-family: "NanumSquare";
+    font-size: 14px;
+    font-weight: 800;
+    padding: 25% 8% 4%;
+    border-bottom: 4px solid #4fe0b6;
+  }
+
+  .NotActive {
+    box-sizing: border-box;
+    width: 50%;
+    border: none;
+    background-color: #ffffff;
+    font-family: "NanumSquare";
+    font-size: 14px;
+    font-weight: 400;
+    padding: 25% 8% 4%;
+    border-bottom: 1px solid #4fe0b6;
+  }
+`;
+
+const Btn = styled.button``;
 
 export default EyeHistoryPage;
