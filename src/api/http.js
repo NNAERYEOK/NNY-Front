7117bce1b4e32feee899d5153ddbda7bff9a567a
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-08-17 14:44:23 +0900

    Update http.js

--- src/api/http.js
+++ src/api/http.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const http = axios.create({ baseURL: "http://cha2y0ung.pythonanywhere.com" });
+const http = axios.create(
+  { baseURL: "http://cha2y0ung.pythonanywhere.com" },
+  { withCredentials: true, crossDomain: true },
+);
 http.defaults.withCredentials = true;
 
 export default http;
