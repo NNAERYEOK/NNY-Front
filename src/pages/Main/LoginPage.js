fd7e06d805a2631307be0285d3dbaa42c84d1c84
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-08-17 12:56:16 +0900

    Update LoginPage.js

--- src/pages/Main/LoginPage.js
+++ src/pages/Main/LoginPage.js
@@ -29,7 +29,7 @@ const LoginPage = () => {
 
     axios.defaults.withCredentials = true;
     axios
-      .post("http://cha2y0ung.pythonanywhere.com/", {
+      .post("https://cha2y0ung.pythonanywhere.com/", {
         email: "1234@1234.com",
         password: "1234",
         withCredentials: true,
