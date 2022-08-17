1fc0173281dd07735572b6123e71f21f7f31c16c
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-08-17 15:36:56 +0900

    Update http.js

--- src/api/http.js
+++ src/api/http.js
@@ -3,10 +3,15 @@ import axios from "axios";
 const http = axios.create({
   baseURL: "https://cha2y0ung.pythonanywhere.com",
   withCredentials: true,
-  headers: {
-    "Access-Control-Allow-Origin": "https://cha2y0ung.pythonanywhere.com", // 서버 domain
-  },
 });
+
+// const http = axios.create({
+//   baseURL: "https://cha2y0ung.pythonanywhere.com",
+//   withCredentials: true,
+//   headers: {
+//     "Access-Control-Allow-Origin": "https://cha2y0ung.pythonanywhere.com", // 서버 domain
+//   },
+// });
 
 
 // const http = axios.create(
